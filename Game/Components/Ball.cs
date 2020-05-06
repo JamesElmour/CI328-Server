1589a7699bc0ad1cc2bdfff5e8712d4da960e14a
James Elmour <61156265+JamesElmour@users.noreply.github.com>
2020-05-06 17:00:12 +0100

    Adding Ball ID and combo counter to Ball component.

--- Game/Components/Ball.cs
+++ Game/Components/Ball.cs
@@ -9,6 +9,8 @@ namespace PIGMServer.Game.Components
         public float Speed = 600;
         public bool WasCollidingWithPlayer = false;
         public bool WasCollidingWithBrick = false;
+        public int Combo = 0;
+        public int ID = 0;
 
         public Ball(GameEntity parent) : base(parent)
         {
